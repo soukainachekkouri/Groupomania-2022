f9cf5c3551e224f46254e7e44d3d00e92194c2ce
soukainachekkouri <58041264+soukainachekkouri@users.noreply.github.com>
2022-10-25 20:00:30 +0200

    maj

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,16 +2,13 @@ import "../Header/Header.css";
 import logo from "../Header/assets/logo.png";
 import avatar from "../Header/assets/avatar.png";
 import { NavLink } from "react-router-dom";
+import ProfileButton from "./ProfileButton";
 
 function Header() {
   return (
     <div className="lmj-header" id="navbarLogged">
       <img src={logo} alt="Gropomania" className="lmj-logo" />
-      <h1 className="lmj-page-title"> Les posts </h1>{" "}
-      <button className="lmj-profile-button">
-        <h1 className="lmj-button-name"> Mon profil </h1>{" "}
-        <img src={avatar} alt="My-avatar" className="lmj-button-avatar" />
-      </button>{" "}
+      <h1 className="lmj-page-title"> Les posts </h1> <ProfileButton />
     </div>
   );
 }
---
+++ frontend/src/components/Header/ProfileButton.jsx
@@ -0,0 +1,42 @@
+import Dropdown from "react-bootstrap/Dropdown";
+import React from "react";
+import "../Header/ProfileButton.css";
+import avatar from "../Header/assets/avatar.png";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const ProfileButton = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <div className="Dropdown-menu">
+      <button onClick={handleOpen} className="dropdown-button">
+        Profil
+        <img src={avatar} alt="Gropomania" className="dropdown-button-avatar" />
+      </button>{" "}
+      {open ? (
+        <ul className="menu">
+          <li className="menu-item">
+            <button>
+              <FontAwesomeIcon icon={faUser} />
+              Mon profil
+            </button>
+          </li>
+          <li className="menu-item">
+            <button>
+              <FontAwesomeIcon icon={faArrowRightFromBracket} />
+              Déconnexion
+            </button>
+          </li>
+        </ul>
+      ) : null}
+    </div>
+  );
+};
+
+export default ProfileButton;
